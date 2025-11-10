8e5461bea3e8c71ff7b85684dfcdfcbff2a85888
Juan Saavedra <saavedrajuanpis@gmail.com>
2025-11-10 16:10:42 -0500

    Refactor(auth): remove auth module and simplify auth routing.

--- Fronted/Fronted-Web/Inventory/src/app/auth/auth.module.ts
+++ Fronted/Fronted-Web/Inventory/src/app/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -10,12 +9,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
+import { AuthRoutingModule } from './auth-routing.module';
+
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterModule,
+    AuthRoutingModule,
     // Angular Material
     MatCardModule,
     MatFormFieldModule,
